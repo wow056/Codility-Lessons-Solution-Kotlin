d2cb95ba513717ed4d73b9e5a039446be8ee145b
Minsu_Kang <wow056@naver.com>
2022-02-12 12:13:17 +0900

    Update FrogRiverOne.kt

--- lesson4-counting-elements/FrogRiverOne.kt
+++ lesson4-counting-elements/FrogRiverOne.kt
@@ -1,24 +1,10 @@
 fun solution(X: Int, A: IntArray): Int {
-    // write your code in Kotlin 1.3.11 (Linux)
-    val isLeafAppears = MutableList(X) {
-        false
-    }
-    var lastIsLeafAppearsIndex = 0
+    val countSet = mutableSetOf<Int>()
     A.forEachIndexed { index, item ->
-        isLeafAppears[item - 1] = true
-        val updatedIndex = updateIndex(lastIsLeafAppearsIndex, isLeafAppears)
-        if (updatedIndex == isLeafAppears.lastIndex) {
+        countSet.add(item)
+        if (countSet.size == X) {
             return index
         }
     }
     return -1
-}
-
-fun updateIndex(prevIndex: Int, isLeafAppears: List<Boolean>): Int {
-    for (i in prevIndex..(isLeafAppears.lastIndex)) {
-        if (!isLeafAppears[i]) {
-            return i - 1
-        }
-    }
-    return isLeafAppears.lastIndex
 }
